da4bab7b6943cfe6c855e4f083b51c75a249d067
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2021-01-30 19:15:29 -0300

    project created

---
+++ spoilerapp-network/src/main/java/com/eduardo/spoilerappnetwork/SpoilerappNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.spoilerappnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoilerappNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpoilerappNetworkApplication.class, args);
+    }
+
+}
---
+++ spoilerapp-network/src/test/java/com/eduardo/spoilerappnetwork/SpoilerappNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.spoilerappnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoilerappNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
